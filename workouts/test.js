36010e83f4ab5944f8464eef683b2ff07b54cba0
mattedwards <the.matt.edwards@googlemail.com>
2023-05-17 22:16:35 +0100

    Add a test workout

---
+++ workouts/test.js
@@ -0,0 +1,12 @@
+const workout={
+  description:'Development test workout',
+  steps:[
+    {duration:15,watts:85},
+    {duration:15,watts:100},
+    {duration:15,watts:120},
+    {duration:15,watts:140},
+    {duration:120,watts:200},
+    {duration:60,watts:100},
+    {duration:60,watts:50}
+  ]
+}
