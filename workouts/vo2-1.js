2657520318c442b4801e033558742cf64e092713
mattedwards <the.matt.edwards@googlemail.com>
2023-05-25 08:37:26 +0100

    update vo2-1 workout schema

--- workouts/vo2-1.js
+++ workouts/vo2-1.js
@@ -1,7 +1,9 @@
 const workout={
   description: 'VO2 - 2 sets of (4 x 1 min work interval with 2 min recovery). 5 min between sets.',
   steps: [
+    { duration: 420, watts: 50 },
     { duration: 180, watts: 85, cadence: '85-95' },
+    { duration: 60, watts: 60 },
     { duration: 120, watts: 105, cadence: '85-95' },
     { duration: 30, watts: 145, cadence: '90-105' },
     { duration: 60, watts: 85, cadence: '85-95' },
